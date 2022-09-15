f2a88670ee4f0909e5746d4c6e25852081a400b6
Vincius Silva <vinilrsilv@gmail.com>
2022-09-15 08:50:49 -0300

    [First Project]: Exploring data types

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -6,7 +6,34 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //SByte x = 100;
+            //Console.WriteLine(x);
+            //// 100
+
+            //byte n1 = 255;
+            //n1 = -255;
+            // Error
+
+            //n1++;
+            //// Returns 0
+
+            SByte x = 100;
+            byte n1 = 255;
+            int n2 = 2147483647;
+            long n3 = 2147483648L;
+            float n4 = 4.5f;
+            double n5 = 4.5;
+
+            char initial = 'V';
+            char unicodeLetter = '\u0041';
+            string name = "Joe";
+
+            bool isTrue = false;
+
+            object obj1 = "Alex";
+
+            Console.WriteLine(obj1);
+
         }
     }
 }
