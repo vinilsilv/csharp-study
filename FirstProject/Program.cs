1d09191d352a4dc40b9e788c43f0423e31bd47ea
Vinicius Silva <vinilrsilv@gmail.com>
2022-09-25 18:58:51 -0300

    [First Project]: Using parse on data input

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace FirstProject
 {
@@ -17,27 +18,27 @@ namespace FirstProject
             //n1++;
             //// Returns 0
 
-            SByte x = 100;
-            byte n1 = 255;
-            int n2 = 2147483647;
-            long n3 = 2147483648L;
-            float n4 = 4.5f;
-            double n5 = 4.5;
-
-            char initial = 'V';
-            char unicodeLetter = '\u0041';
-            string name = "Joe";
-
-            bool isTrue = false;
-
-            object obj1 = "Alex";
-
-
-            int maxVal = int.MaxValue;
-            int minVal = int.MinValue;
+            // SByte x = 100;
+            // byte n1 = 255;
+            // int n2 = 2147483647;
+            // long n3 = 2147483648L;
+            // float n4 = 4.5f;
+            // double n5 = 4.5;
+             
+            // char initial = 'V';
+            // char unicodeLetter = '\u0041';
+            // string name = "Joe";
+             
+            // bool isTrue = false;
+             
+            // object obj1 = "Alex";
+             
+             
+            // int maxVal = int.MaxValue;
+            // int minVal = int.MinValue;
             // Works for int, sbyte, long, decimal, etc...
 
-            int age = 40;
+            // int age = 40;
 
             // Console.WriteLine("{0} is {1} years old.", name, age);
             // Console.WriteLine($"{name} is {age} years old.");
@@ -45,9 +46,23 @@ namespace FirstProject
 
             // string phrase = Console.ReadLine();
 
-            string[] vet = Console.ReadLine().Split(' ');
+            // string[] vet = Console.ReadLine().Split(' ');
 
-            Console.WriteLine(vet[0]);
+            // int num1 = int.Parse(Console.ReadLine());
+            // char ch = char.Parse(Console.ReadLine());
+            // double num2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string name = vet[0];
+            char gender = char.Parse(vet[1]);
+            int age = int.Parse(vet[2]);
+            double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(name);
+            Console.WriteLine(gender);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
         }
     }
 }
