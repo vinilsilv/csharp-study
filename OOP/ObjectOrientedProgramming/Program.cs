8c953f46ad4904c3d1c821450ba0078b6ddfbcb8
Vinicius Silva <vinilrsilv@gmail.com>
2022-10-07 06:31:07 -0300

    [OOP]: Creating a reusable method inside the class

--- OOP/ObjectOrientedProgramming/Program.cs
+++ OOP/ObjectOrientedProgramming/Program.cs
@@ -30,11 +30,9 @@ namespace ObjectOrientedProgramming // Note: actual namespace depends on the pro
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (x.A + x.B + x.C) / 2;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            double areaX = x.Area();
             
-            p = (y.A + y.B + y.C) / 2;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            double areaY = y.Area();
 
             Console.WriteLine($"Triangle X's area = {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"Triangle Y's area = {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
--- OOP/ObjectOrientedProgramming/Triangle.cs
+++ OOP/ObjectOrientedProgramming/Triangle.cs
@@ -1,9 +1,14 @@
-﻿namespace ObjectOrientedProgramming
+﻿namespace ObjectOrientedProgramming;
+using System;
+
+internal class Triangle
 {
-    internal class Triangle
-    {
-        public double A;
-        public double B;
-        public double C;
+    public double A;
+    public double B;
+    public double C;
+
+    public double Area() { 
+        double p = (A + B + C) / 2;
+        return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
     }
 }
