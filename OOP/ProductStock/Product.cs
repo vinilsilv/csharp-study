872a3969ab49ca731e9af116e951a59ce198e6b8
Vinicius Silva <vinilrsilv@gmail.com>
2022-10-07 09:10:32 -0300

    [OOP]: Creating a program that registers one product's data using OOP

---
+++ OOP/ProductStock/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ProductStock
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Amount;
+
+        public double TotalValueInStock()
+        {
+            return Price * Amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, R$ {Price.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                $"Amount: {Amount}, Total: R$ {TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public void AddProducts(int add)
+        {
+            Amount += add;
+        }
+
+        public void RemoveProducts(int remove)
+        {
+            Amount -= remove;
+        }
+    }
+}
---
+++ OOP/ProductStock/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProductStock // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            Console.WriteLine("Insert product data: ");
+            Console.Write("Name: ");
+            p.Name = Console.ReadLine();
+
+            Console.Write("Price: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Amount: ");
+            p.Amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Product data: {p}");
+
+            Console.Write("Type the amount of products to be added to the stock: ");
+            int amount = int.Parse(Console.ReadLine());
+            p.AddProducts(amount);
+
+            Console.WriteLine($"Updated stock data: {p}");
+
+            Console.Write("Type the amount of products to be removed from the stock: ");
+            amount = int.Parse(Console.ReadLine());
+            p.RemoveProducts(amount);
+
+            Console.WriteLine($"Updated stock data: {p}");
+        }
+    }
+}
