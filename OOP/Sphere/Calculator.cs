e7cc2b140f9d20cdafd5bd20ae9edee6e2f4b2fb
Vinicius Silva <vinilrsilv@gmail.com>
2022-10-08 05:17:41 -0300

    [OOP]: Delegating methods to a new Calculator class

---
+++ OOP/Sphere/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace Sphere
+{
+    internal class Calculator
+    {
+        public double Pi = 3.14;
+
+        public double Circumference(double radius)
+        {
+            return 2 * Pi * radius;
+        }
+
+        public double Volume(double radius)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(radius, 3);
+        }
+    }
+}
--- OOP/Sphere/Program.cs
+++ OOP/Sphere/Program.cs
@@ -6,31 +6,18 @@ namespace Sphere // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
+            Calculator calc = new Calculator();
+
             Console.WriteLine("Insert radius value: ");
             double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double circ = Circumference(radius);
+            double circ = calc.Circumference(radius);
+            double volume = calc.Volume(radius);
 
             Console.WriteLine("Circumference: " + circ.ToString("F2", CultureInfo.InvariantCulture));
-
-            double volume = Volume(radius);
-
             Console.WriteLine("Volume:" + volume.ToString("F2", CultureInfo.InvariantCulture));
-
-            Console.WriteLine("Value of PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-        }
-
-        static double Circumference(double radius)
-        {
-            return 2 * Pi * radius;
-        }
-
-        static double Volume(double radius)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(radius, 3);
+            Console.WriteLine("Value of PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
